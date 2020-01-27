ae80c02d687f85c9c751d99f5100392f53bca76c
Jasmine Chan <chan.jasmine4@gmail.com>
2020-01-27 21:08:57 +0800

    Added insertion sort

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Common Algorithms
 
-Will continuously update this list of common algorithms for personal reference as I relearn them. Open to advice and suggestions. My aim is to upload one a day.
+Will continuously update this list of common algorithms for study reference as I relearn them. Open to advice and suggestions. My aim is to upload one a day.
 
 - [Euclidean Algorithm](./LCM-GCF.cpp)
 - [Exponential Square Algorithm](./exponential-squaring.cpp)
 - Permutation
   - [Heap's Algorithm](./heaps-algorithm.cpp)
-  - [Simple backtracking](perm-backtracking.cpp)
+  - [Simple backtracking](./perm-backtracking.cpp)
+- Sorting
+  - [Insertion Sort](./insertion-sort.cpp)
---
+++ insertion-sort.cpp
@@ -0,0 +1,49 @@
+/*
+    Insertion sort
+    The algorithm sorts the input numbers in place.
+*/
+
+#include<iostream>
+#include<vector>
+
+// Overload output function to print array
+std::ostream& operator<< (std::ostream &out, const std::vector<int> &arr) {
+	int size = arr.size();
+	for(int i = 0; i < size; i++) {
+		out << arr[i];
+		if(i != size-1) {
+			out << ", ";
+		}
+	}
+	return out;
+}
+
+// Sorts vector passed in input using the insertion sort
+void insertionSort(std::vector<int> &arr) {
+    int val = 0;
+    int index = 0;
+    for(int i = 1; i < arr.size(); i++) {
+        val = arr[i];
+        index = i;
+        for(int j = i-1; j >= 0; j--) {
+            if(arr[j] > val) {
+                std::swap(arr[j], arr[index]);
+                index = j;
+            }
+        }
+    }
+}
+
+int main() {
+    std::vector<int> arr;
+    arr.push_back(5);
+    arr.push_back(3);
+    arr.push_back(16);
+    arr.push_back(11);
+    arr.push_back(21);
+    arr.push_back(2);
+
+    insertionSort(arr);
+    std::cout << arr << std::endl;
+    return 0;
+}
